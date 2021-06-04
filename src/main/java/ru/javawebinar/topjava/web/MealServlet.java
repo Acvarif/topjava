75053504d281b2889fed09a920854aad17b790da
Yury <yguvin@gmail.com>
2021-06-04 08:40:40 +0300

    HW1 create table meals

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.TimeUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    private List<Meal> meals = Arrays.asList(
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    );
+
+    private static int caloriesPerDay;
+
+    static {
+        caloriesPerDay = 2000;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meals");
+
+        final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
+        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+
+        List<MealTo> mealTos = new ArrayList<>();
+        for (Meal meal : meals) {
+            MealTo mealTo = new MealTo(meal.getDateTime(), meal.getDescription(),
+                    meal.getCalories(),
+                    caloriesSumByDate.get(meal.getDateTime().
+                            toLocalDate()) > caloriesPerDay);
+            mealTos.add(mealTo);
+        }
+
+        request.setAttribute("meals", mealTos);
+
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,72 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html lang="ru">
+<head>
+    <title>Meals</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            width: 50%;
+            padding: 10px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+    <table >
+        <thead>
+        <tr>
+            <th>
+                Date
+            </th>
+            <th>
+                Description
+            </th>
+            <th>
+                Calories
+            </th>
+            <th>
+
+            </th>
+            <th>
+
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${meals}" var="meal">
+            <c:choose>
+                <c:when test="${meal.excess}">
+                    <c:set var = "color" value = "red"/>
+                </c:when>
+                <c:otherwise>
+                    <c:set var = "color" value = "green"/>
+                </c:otherwise>
+            </c:choose>
+            <tr style="color: ${color}">
+                <td>
+                    ${meal.dateTime}
+                </td>
+                <td>
+                    ${meal.description}
+                </td>
+                <td>
+                    ${meal.calories}
+                </td>
+                <td>
+
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+
+</body>
+</html>
