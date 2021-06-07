34a18f5050428754d0c0a38ba8e5fe2ef0762e09
Yury <yguvin@gmail.com>
2021-06-07 14:22:02 +0300

    HW1 home work table meals with excess - correction

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
 
@@ -18,6 +20,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+
+    private static Integer CALORIES_PER_DAY = 2000;
 
     private List<Meal> meals = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
@@ -29,31 +33,14 @@ public class MealServlet extends HttpServlet {
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
     );
 
-    private static int caloriesPerDay;
-
-    static {
-        caloriesPerDay = 2000;
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
-        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
-
-        List<MealTo> mealTos = new ArrayList<>();
-        for (Meal meal : meals) {
-            MealTo mealTo = new MealTo(meal.getDateTime(), meal.getDescription(),
-                    meal.getCalories(),
-                    caloriesSumByDate.get(meal.getDateTime().
-                            toLocalDate()) > caloriesPerDay);
-            mealTos.add(mealTo);
-        }
+        List<MealTo> mealTos = MealsUtil.filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
 
         request.setAttribute("meals", mealTos);
 
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
-//        response.sendRedirect("meals.jsp");
     }
 }
