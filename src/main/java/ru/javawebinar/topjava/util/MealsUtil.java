2df8d27f6410ab618651b5e69aa5696cb5cc7566
Yury <yguvin@gmail.com>
2021-06-10 09:41:25 +0300

    HW0original

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 public class MealsUtil {
     public static void main(String[] args) {
         List<Meal> meals = Arrays.asList(
