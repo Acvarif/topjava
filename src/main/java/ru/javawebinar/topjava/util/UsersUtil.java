4798d79d3a02d31799f1fb49a724fdbf2413583b
Yury <yguvin@gmail.com>
2021-06-12 21:12:18 +0300

    HWtest

--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -5,8 +5,14 @@ import ru.javawebinar.topjava.to.UserTo;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UsersUtil {
+
+//    public static List<UserTo> filterByPredicate(Collection<User> users) {
+//        return users.stream()
+//                .collect(Collectors.toList());
+//    }
 
     public static UserTo createTo(User user) {
         return new UserTo(user.getName(), user.getEmail(), user.getPassword(), user.getCalories());
